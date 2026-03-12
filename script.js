e1092b7c589036bc8b2b8e088cedddd560892c2a
RKCapital0018 <kautuk0018@gmail.com>
2026-03-13 01:55:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -217,3 +217,18 @@ window.location.href =
 },500);
 
 }
+function toggleMenu(){
+
+let menu=document.getElementById("navMenu");
+
+if(menu.style.display==="block"){
+
+menu.style.display="none";
+
+}else{
+
+menu.style.display="block";
+
+}
+
+}
